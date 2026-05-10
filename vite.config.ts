dfd7db3c7d3ee24e4894f39588a71fa39a8aa7fa
arielkinge541-crypto <arielkinge541@gmail.com>
2026-05-10 17:26:34 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   base: "/true-crime-chronicle/",
   ssr: false,
-  tanstackStart: {
-  },
+  tanstackStart: { },
 });
